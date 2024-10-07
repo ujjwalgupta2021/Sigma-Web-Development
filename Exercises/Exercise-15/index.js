fe08c727cb59a57ff525ec686eff9cdc47b28dcc
ujjwalgupta2021 <ujjwalgupta5750@gmail.com>
2024-10-07 05:37:32 +0530

    Added solution

--- Exercises/Exercise-15/index.js
+++ Exercises/Exercise-15/index.js
@@ -19,3 +19,28 @@ zip/harry.zip zip/Rohan.zip
 */
 
 
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
+
+const basepath = "E:\\Programs VS Code\\Sigma-Web-Development\\Exercises\\Exercise-15"
+
+let files = await fs.readdir(basepath)
+
+for (const item of files) {
+    console.log("running for ", item)
+    let ext = item.split(".")[item.split(".").length - 1]
+    if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+
+
+        if (fsn.existsSync(path.join(basepath, ext))) {
+            // Move the file to this directory if its not a js or json file
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+        else {
+            fs.mkdir(ext)
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+    }
+
+}
