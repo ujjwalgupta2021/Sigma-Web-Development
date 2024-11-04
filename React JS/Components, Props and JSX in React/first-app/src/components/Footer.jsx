1930061068102e848b2e0b1daf15ef156bdb35ed
ujjwalgupta2021 <ujjwalgupta5750@gmail.com>
2024-11-04 21:49:54 +0530

    Components, Props and JSX in React

---
+++ React JS/Components, Props and JSX in React/first-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy; www.website.com | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
