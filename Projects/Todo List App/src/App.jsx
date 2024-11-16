3e2e8a6587fad7162fa1afc3cbf15238a02452a7
ujjwalgupta2021 <ujjwalgupta5750@gmail.com>
2024-11-16 21:40:05 +0530

    Added Navbar

--- Projects/Todo List App/src/App.jsx
+++ Projects/Todo List App/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <Navbar/>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world! </h1>
       </div>
