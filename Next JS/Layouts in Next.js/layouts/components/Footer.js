12b63e1d4a66072ba8caf9c7801b12a83543806c
ujjwalgupta2021 <ujjwalgupta5750@gmail.com>
2024-12-03 07:00:53 +0530

    Layouts in Next.js

---
+++ Next JS/Layouts in Next.js/layouts/components/Footer.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4">
+            <div className="container mx-auto">
+                Footer
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
